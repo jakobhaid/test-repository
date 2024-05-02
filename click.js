a48cab2708310c938d61ffa70e4654f9da111cb8
jakob <jakob.haidlauf@gmail.com>
2024-05-02 11:26:42 +0200

    kleinichkeit

--- click.js
+++ click.js
@@ -1,5 +1,6 @@
 var lebelList = document.getElementsByTagName("label");
 
 for (let index = 0; index < lebelList.length; index++) {
-    lebelList[index].addEventListener("click", function(event){alert(this.innerText);});
+    lebelList[index].addEventListener("click", function(event){alert(lebelList[index].textContent);});
+    console.log(lebelList[index]);
 }
